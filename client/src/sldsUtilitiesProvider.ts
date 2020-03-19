597793f850a67e2240f8b178aa1a7ef23be8b3f4
Ambreen Hasan <ahasan@salesforce.com>
2020-03-19 12:19:50 -0700

    FIx tsconfig

--- client/src/sldsUtilitiesProvider.ts
+++ client/src/sldsUtilitiesProvider.ts
@@ -17,38 +17,38 @@ const triggerChars = 'abcdefghijklmnopqrstuvwxyz1234567890-_'.split('');
 // determine if current state warrants looking for completion items
 // if yes return range of text that would be replaced, if no return undefined
 function shouldTriggerCompletions(document: vscode.TextDocument, position: vscode.Position): vscode.Range | undefined {
-	// must start with "slds-"
-	let triggerRange = document.getWordRangeAtPosition(position, /\bslds-[\w-]*/i);
-	return triggerRange;
+  // must start with "slds-"
+  const triggerRange = document.getWordRangeAtPosition(position, /\bslds-[\w-]*/i);
+  return triggerRange;
 }
 
 // get list of completion items to display, potentially replacing the provided text range
 function getCompletions(range: vscode.Range): vscode.CompletionList {
-	let completions = utilities.classes.map(cssClass => {
-		let completionItem = new vscode.CompletionItem(cssClass.selector);
-		completionItem.kind = vscode.CompletionItemKind.Value;
-		completionItem.detail = cssClass.component;
-		completionItem.documentation = cssClass.summary;
+  const completions = utilities.classes.map(cssClass => {
+    const completionItem = new vscode.CompletionItem(cssClass.selector);
+    completionItem.kind = vscode.CompletionItemKind.Value;
+    completionItem.detail = cssClass.component;
+    completionItem.documentation = cssClass.summary;
 
-		// some langs treat "-" as a word separator and so they won't replace "slds-" upon completion commit
-		// this sets the replaced range to what we regex'd earlier so the "slds-" is included when replacing
-		completionItem.range = range;
+    // some langs treat "-" as a word separator and so they won't replace "slds-" upon completion commit
+    // this sets the replaced range to what we regex'd earlier so the "slds-" is included when replacing
+    completionItem.range = range;
 
-		return completionItem;
-	});
+    return completionItem;
+  });
 
-	return new vscode.CompletionList(completions, false);
+  return new vscode.CompletionList(completions, false);
 }
 
 export function register(context: vscode.ExtensionContext): vscode.Disposable {
-	const extensionContext: vscode.ExtensionContext = context;
-	const provider = {
-		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-			let triggerRange = 
+  const extensionContext: vscode.ExtensionContext = context;
+  const provider = {
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+      const triggerRange = 
 				SLDSContext.isEnable(extensionContext, ContextKey.GLOBAL, ContextKey.AUTO_SUGGEST, ContextKey.UTILITY_CLASS) 
-					? shouldTriggerCompletions(document, position) : false;
-			return triggerRange ? getCompletions(triggerRange) : undefined;
-		}
-	};
-	return vscode.languages.registerCompletionItemProvider(documentSelector, provider, ...triggerChars);
+				  ? shouldTriggerCompletions(document, position) : false;
+      return triggerRange ? getCompletions(triggerRange) : undefined;
+    }
+  };
+  return vscode.languages.registerCompletionItemProvider(documentSelector, provider, ...triggerChars);
 }
