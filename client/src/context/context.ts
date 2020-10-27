f4010b6c8e80ddff2f24f1746b2ff9168a4abb87
Billy Ma <bma@salesforce.com>
2020-10-27 11:14:12 -0700

    Transition to Settings

--- client/src/context/context.ts
+++ client/src/context/context.ts
@@ -9,50 +9,63 @@ import * as vscode from 'vscode';
 import { ContextKey } from './contextKey';
 import { LanguageClient } from 'vscode-languageclient';
 
-const CONFIG_KEY: string = "salesforcedx-vscode-slds";
+const CONFIG_KEY: string = 'salesforcedx-vscode-slds';
 
 const MAPPINGS: Map<ContextKey, string> = new Map<ContextKey, string>([
-	[ContextKey.GLOBAL, "enabled"], 
-	[ContextKey.AUTO_SUGGEST, "enhancements.autoSggest"],
-	[ContextKey.BEM, "basic.bemNaming"],
-	[ContextKey.DENSITY, "enhancements.density"],
-	[ContextKey.DEPRECATED, "basic.deprecated"],
-	[ContextKey.OVERRIDE, "basic.override"],
-	[ContextKey.DESIGN_TOKEN, "basic.designToken"],
-	[ContextKey.INVALID, "basic.invalid"],
-	[ContextKey.UTILITY_CLASS, "enhancements.utilityClasses"],
-	[ContextKey.SCOPE, "file.scopeWithInSFDX"]
+	[ContextKey.GLOBAL, 'enabled'], 
+	[ContextKey.AUTO_SUGGEST, 'enhancements.autoSuggest'],
+	[ContextKey.BEM, 'basic.bemNaming'],
+	[ContextKey.DENSITY, 'enhancements.density'],
+	[ContextKey.DEPRECATED, 'basic.deprecated'],
+	[ContextKey.OVERRIDE, 'basic.override'],
+	[ContextKey.DESIGN_TOKEN, 'basic.designToken'],
+	[ContextKey.INVALID, 'basic.invalid'],
+	[ContextKey.UTILITY_CLASS, 'enhancements.utilityClasses'],
+	[ContextKey.SCOPE, 'file.scopeWithInSFDX']
 ]);
 
 export class SLDSContext {
 	context: vscode.ExtensionContext;
 	languageClient: LanguageClient;
+	configuration: vscode.WorkspaceConfiguration;
 
 	constructor(context: vscode.ExtensionContext, languageClient: LanguageClient) {
 		this.context = context;
 		this.languageClient = languageClient;
+		this.configuration = vscode.workspace.getConfiguration(CONFIG_KEY);
 
-		this.syncServer();
-
-		const self = this;
-
-		vscode.workspace.onDidChangeConfiguration((event: vscode.ConfigurationChangeEvent) => {
-			for (let [key, value] of MAPPINGS) {
-
-				if (event.affectsConfiguration(`${CONFIG_KEY}${value}`) &&
-					SLDSContext.shouldNotifyServerOfContextChange(key)) {
-
-					self.languageClient.onReady().then(() => {
-						self.languageClient.sendNotification('state/updateState', { key, value });
-					});
-				}
-			}
-		});
+		this.upgradeContextConfiguration(context);
+		this.syncContextWithServer(MAPPINGS.keys());
+		vscode.workspace.onDidChangeConfiguration((e) => this.configurationChangeHandler(e));
 	}
 
-	private syncServer(): void {
+	private upgradeContextConfiguration(context: vscode.ExtensionContext): void {
+		for (let [key] of MAPPINGS) {
+			const result: boolean = context.globalState.get(key);
+			if (result !== undefined) {
+				this.updateState(key, result);
+				context.globalState.update(key, undefined);
+			}
+		}
+	}
+
+	private configurationChangeHandler(event: vscode.ConfigurationChangeEvent) : void {
+		const keys: Array<ContextKey> = [];
+
+		for (let [key, value] of MAPPINGS) {
+			if (event.affectsConfiguration(`${CONFIG_KEY}.${value}`)) {
+				keys.push(key);		
+			}
+		}
+
+		if (keys.length !== 0) {
+			this.syncContextWithServer(keys);
+		}
+	}
+
+	private syncContextWithServer(keys: Iterable<ContextKey>): void {
 		this.languageClient.onReady().then(() => {
-			for (var key in ContextKey) {
+			for (let key of keys) {
 				const contextKey: ContextKey = <ContextKey>key;
 				const value = SLDSContext.isEnable(contextKey);
 				if (SLDSContext.shouldNotifyServerOfContextChange(contextKey)) {
@@ -60,10 +73,12 @@ export class SLDSContext {
 				}
 			}
 		});
-	}
+	}  
 
 	public updateState(key: ContextKey, value: boolean) {
-		vscode.workspace.getConfiguration("salesforcedx-vscode-slds").update(MAPPINGS.get(key), value);
+		//accounting for situtation where workspace is not available
+		const setGlobal: boolean = vscode.workspace.name === undefined;
+		this.configuration.update(MAPPINGS.get(key), value, setGlobal);
 	}
 
 	private static shouldNotifyServerOfContextChange(key: ContextKey): boolean {
@@ -71,12 +86,12 @@ export class SLDSContext {
 	}
 
 	public static isEnable(...keys: ContextKey[]): boolean {
-		const workspace: vscode.WorkspaceConfiguration =  vscode.workspace.getConfiguration("salesforcedx-vscode-slds");
+		const workspace: vscode.WorkspaceConfiguration =  vscode.workspace.getConfiguration(CONFIG_KEY);
 
 		for (let key of keys) {
 			const value: boolean = workspace.get<boolean>(MAPPINGS.get(key));
 
-			if (value === false) {
+			if (!!! value ) {
 				return false;
 			}
 		}
